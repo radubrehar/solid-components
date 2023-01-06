5d0c250cc366b51c5809f76196aa504c20cbc66d
radubrehar <radu@jslog.com>
2023-01-07 00:51:41 +0200

    add solid start

---
+++ packages/www/src/routes/calendar.tsx
@@ -0,0 +1,12 @@
+import { Calendar } from "@rb/solid-calendar";
+
+export default function CalendarPage() {
+  return (
+    <main class="text-center mx-auto text-gray-700 p-4">
+      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
+        Calendar Page
+      </h1>
+      <Calendar />
+    </main>
+  );
+}
